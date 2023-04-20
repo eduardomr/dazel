e9ba84a8054e71268104eaa5d36c1e4c7978b414
Eduardo <emaiarezende@gmail.com>
2023-04-20 20:06:06 -0300

    Level setup

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame,sys
+from settings import *
+from debug import debug
+from level import Level
+
+class Game:
+    def __init__(self):
+
+        #general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('JOGUINHO DE BOIOLA')
+
+        self.level = Level()
+
+    def run(self):
+        while(True):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.quit()
+                    sys.exit()
+                
+                self.screen.fill('black')
+                self.level.run()
+                pygame.display.update()
+                self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
